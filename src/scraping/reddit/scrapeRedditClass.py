ecbb73d00468f21938071f6fbe2117b69461a81d
Randy Chan <randy.chan@bitopiatech.com>
2022-06-27 17:26:15 +0800

    revamped scrape to combine df

--- src/scraping/reddit/scrapeRedditClass.py
+++ src/scraping/reddit/scrapeRedditClass.py
@@ -7,7 +7,7 @@ from sqlalchemy import create_engine
 import io
 import os
 
-ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+
 
 
 
@@ -61,45 +61,6 @@ class Scrape_subreddit:
         return self.dataframe
 
    
-def getRedditData(reddit,subreddit,tag,limit):
-
-    data = scrape(reddit, subreddit, tag, limit)
-    # print(data)
-    writeToCSV(data, tag)
-    return data
 
 
 
-
-def writeToCSV(df, tag):
-    #to save to postgres directly
-    # engine = create_engine('postgresql://postgres:password1@localhost:5432/test')
-    # df.to_sql(tag, engine, if_exists='replace',index=False)
-    
-    
-    filepath = os.path.join(ROOT_DIR , 'csvFiles')
-    df.to_csv(filepath + '/reddit' + '.csv', sep='|', index=False,header=False,encoding='utf-8')
-    
-    
-    
-def scrape(reddit,subreddit,tag,limit):
-    reddit = reddit
-    scrape = Scrape_subreddit(reddit,subreddit,tag,limit)
-    getDataFrame = scrape.get_dataframe()
-    return getDataFrame
-
-def scrapeMultipleData(subreddit,tagList,limit,**kwargs):
-    # Creating a reddit crawler object - your own user name and password
-    reddit = praw.Reddit(client_id='cuDitSOXcwUFJxABTQR93Q',
-                        client_secret='BN-dhMP04h9hAQB6ZXzfui4ku7faZQ',
-                        user_agent='jamiescrapes',
-                        username='JamieRan112',
-                        password='Harrogates@@112')
-    
-    print(reddit.user.me())
-    for tag in tagList:
-        getRedditData(reddit,subreddit,tag,limit)
-        print( f"Dataframe with subreddit {tag} saved into CSV")
-    return True
-
-scrapeMultipleData('singapore', ['bitcoin', 'ethereum', 'cardano', 'zilliqa', 'polygon', 'affyn'], 500)
